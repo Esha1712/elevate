1e9177b85f6b652edd7aa8e641c94bcf7c4b66c9
Esha1712 <krithikaesha@gmail.com>
2025-12-14 15:33:17 +0530

    created star and its animation using each keyframes

--- Documents/animation-int/src/components/ElevateAnimation.tsx
+++ Documents/animation-int/src/components/ElevateAnimation.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+import styles from './ElevateAnimation.module.css';
+
 const ElevateAnimation = () => {
-  return (
-    <div>
-      Elevate
-    </div>
-  )
+    const [key, setKey] = useState(0);
+    return (
+        <div className={styles.animationContainer}>
+            <div key={key} className={styles.shapeWrapper}>
+                <div className={styles.star}></div>
+                <div>FUNNEL</div>
+                <h1>E L E V A T E</h1>
+            </div>
+        </div>
+    )
 }
 
 export default ElevateAnimation
